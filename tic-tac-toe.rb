3548d290148e565314838e500f7a0b8cff7f9464
vmg <musaravgd@gmail.com>
2024-06-10 21:29:33 +0200

    Added game class and created an instance of the Game class and called the play method to start the game

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Class Design
 # Player class
 class Player
@@ -17,8 +19,8 @@ class Board
 
   def display
     @grid.each do |row|
-      puts row.join("|")
-      puts "--+--+---"
+      puts row.join('|')
+      puts '--+--+---'
     end
   end
 
@@ -29,27 +31,69 @@ class Board
 
   def valid_move?(position)
     row, col = position
-    @grid[row][col] == " "
+    @grid[row][col] == ' '
   end
 
   def full?
-    @grid.flatten.none?  { |cell| cell == " "}
+    @grid.flatten.none?  { |cell| cell == ' ' }
   end
 
   def winner?
     winning_combination.any? do |combo|
-        [@grid[combo[0][0]][combo[0][1]], @grid[combo[1][0]][combo[1][1]], 
-         @grid[combo[2][0]][combo[2][1]]].uniq.length == 1 &&
-          @grid[combo[0][0]][combo[0][1]] != " "
+      [@grid[combo[0][0]][combo[0][1]], @grid[combo[1][0]][combo[1][1]],
+       @grid[combo[2][0]][combo[2][1]]].uniq.length == 1 &&
+        @grid[combo[0][0]][combo[0][1]] != ' '
     end
   end
 
   private
 
   def winning_combinations
-    rows = [[0,0], [0,1], [0,2]], [[1,0], [1,1], [1,2]], [[2,0], [2,1], [2,2]]
-    cols = [[0,0], [1,0], [2,0]], [[0,1], [1,1], [2,1]], [[0,2], [1,2], [2,2]]
-    diags = [[0,0], [1,1], [2,2]], [[0,2], [1,1], [2,0]]
+    rows = [[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], [[2, 0], [2, 1], [2, 2]]
+    cols = [[0, 0], [1, 0], [2, 0]], [[0, 1], [1, 1], [2, 1]], [[0, 2], [1, 2], [2, 2]]
+    diags = [[0, 0], [1, 1], [2, 2]], [[0, 2], [1, 1], [2, 0]]
     rows + cols + diags
   end
 end
+
+class Game
+  def initialize
+    @board = Board.new
+    @players = [Player.new('X'), Player.new('O')]
+    @current_player = @players[0]
+  end
+
+  def play
+    loop do
+      @board.display
+      puts "Player #{@current_player.symbol}, enter your move (row and column): "
+      move = gets.chomp.split.map(&:to_i)
+
+      if @board.valid_move?(move)
+        @board.update(move, @current_player.symbol)
+        if @board.winner?
+          @board.display
+          puts "Player #{@current_player.symbol} wins!"
+          break
+        elsif @board.full?
+          @board.display
+          puts "It's a draw!"
+          break
+        else
+          switch_player
+        end
+      else
+        puts 'Invalid move! Try again.'
+      end
+    end
+  end
+
+  private
+
+  def switch_player
+    @current_player = @current_player == @players[0] ? @players[1] : @players[0]
+  end
+end
+
+game = Game.new
+game.play
